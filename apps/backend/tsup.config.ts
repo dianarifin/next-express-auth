67b176fc300d08e1d0fc92948ece3fb26bb463f3
dianarifin <ngawidian@gmail.com>
2026-07-03 06:00:06 +0700

    fix failed build backend

---
+++ apps/backend/tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  clean: true,
+  minify: true,
+  noExternal: ["@repo/database"],
+  external: [
+    "dotenv",
+    "@prisma/client",
+    "@prisma/adapter-pg",
+    "pg",
+  ],
+});
